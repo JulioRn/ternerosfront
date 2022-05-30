428429382b79d4d34559a0e4892724334097be9b
JulioRn <julio.roldan@estudiantes.utec.edu.uy>
2022-05-30 17:43:04 -0300

    Graficas

--- src/componentes/Graficas.js
+++ src/componentes/Graficas.js
@@ -14,12 +14,17 @@ import { useEffect } from 'react';
 import './Principal.css';
 
 
- const Graficas = () => {
+const Graficas = () => {
 
-    
-    
+    const [terneros, setTerneros] = useState([]);
+    const [guacheras, setGuacheras] = useState([]);
+    const [enfermedades, setEnfermedades] = useState([]);
+
+
     useEffect(() => {
         TernerosGet();
+        GuacherasGet();
+        EnfermedadesGet();
     }, [])
 
     const TernerosGet = () => {
@@ -27,21 +32,39 @@ import './Principal.css';
             .then(res => res.json())
             .then(
                 (result) => {
-                    setName(result)
+                    setTerneros(result)
                 }
-
-                
             )
     }
 
-    const [name, setName] = useState([]);
+    const GuacherasGet = () => {
+        fetch("http://localhost:8080/guachera/getAll")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setGuacheras(result)
+                }
+            )
+    }
 
-    const [chartData] = useState({
+    const EnfermedadesGet = () => {
+        fetch("http://localhost:8080/enfermedad/getAll")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setEnfermedades(result)
+                }
+            )
+    }
+
+
+
+    const chartDataRegistros = {
         labels: ['Terneros', 'Guacheras', 'Enfermedades'],
-        
+
         datasets: [
             {
-                data: [name, 50, 50],
+                data: [terneros.length, guacheras.length, enfermedades.length],
                 backgroundColor: [
                     "#FF6384",
                     "#36A2EB",
@@ -53,7 +76,37 @@ import './Principal.css';
                     "#FFCE56"
                 ]
             }]
-    });
+    };
+
+
+    let sum= 0;
+
+    for(let i=0; i<terneros.length; i++) {
+        if(terneros[i].enfermedad.id_enfermedad != null);
+        sum += terneros[i].enfermedad.id_enfermedad
+        console.log(sum)
+      }
+
+   
+
+    const chartData = {
+        labels: ['Terneros', 'Guacheras', 'Enfermedades'],
+
+        datasets: [
+            {
+                data: [terneros.length, guacheras.length, enfermedades.length],
+                backgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ],
+                hoverBackgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ]
+            }]
+    };
 
     const [lightOptions] = useState({
         plugins: {
@@ -66,124 +119,124 @@ import './Principal.css';
     });
 
 
-  return (
+    return (
 
-<div className="surface-0">
-<ResponsiveAppBar />
-<br />
-    <div className="text-700 font-bold text-4xl mb-4 text-center">Monitoreo para la crianza de terneros</div>
-    <br />
-    <div className="grid">
-        <div className="col-12 lg:col-4">
-            <div className="p-3 h-full">
-                <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
-                <div className="text-900 font-medium text-xl mb-2">USUARIOS</div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <div className="flex align-items-center">
-                        <span className="ml-2 font-medium text-600">Descripción</span>
+        <div className="surface-0">
+            <ResponsiveAppBar />
+            <br />
+            <div className="text-700 font-bold text-4xl mb-4 text-center">Monitoreo para la crianza de terneros</div>
+            <br />
+            <div className="grid">
+                <div className="col-12 lg:col-4">
+                    <div className="p-3 h-full">
+                        <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
+                            <div className="text-900 font-medium text-xl mb-2">USUARIOS</div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <div className="flex align-items-center">
+                                <span className="ml-2 font-medium text-600">Descripción</span>
+                            </div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Chart type="doughnut" data={chartDataRegistros} options={lightOptions} />
+                            <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Button label="Gestionar Terneros" className="p-button p-component p-button-raised p-button-success" />
+                        </div>
                     </div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <Chart type="doughnut" data={chartData} options={lightOptions}  />
-                    <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <Button label="Gestionar Terneros" className="p-button p-component p-button-raised p-button-success" />
+                </div>
+
+                <div className="col-12 lg:col-4">
+                    <div className="p-3 h-full">
+                        <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
+                            <div className="text-900 font-medium text-xl mb-2">TERNEROS</div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <div className="flex align-items-center">
+                                <span className="ml-2 font-medium text-600">Descripción</span>
+                            </div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Chart type="doughnut" data={chartData} options={lightOptions} />
+                            <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Button label="Gestionar Terneros" className="p-button p-component p-button-raised p-button-success" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 lg:col-4">
+                    <div className="p-3 h-full">
+                        <div className="shadow-2 p-3 flex flex-column" style={{ borderRadius: '6px' }}>
+                            <div className="text-900 font-medium text-xl mb-2">GUACHERAS</div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <div className="flex align-items-center">
+
+                                <span className="ml-2 font-medium text-600">Descripción</span>
+                            </div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Chart type="doughnut" data={chartData} options={lightOptions} />
+                            <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Button label="Gestionar Guacheras" className="p-button p-component p-button-raised p-button-success" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="grid">
+                <div className="col-12 lg:col-4">
+                    <div className="p-3 h-full">
+                        <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
+                            <div className="text-900 font-medium text-xl mb-2">ENFERMEDADES</div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <div className="flex align-items-center">
+                                <span className="ml-2 font-medium text-600">Descripción</span>
+                            </div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Chart type="doughnut" data={chartData} options={lightOptions} />
+                            <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300 mt-auto" />
+                            <Button label="Gestionar Enfermedades" className="p-button p-component p-button-raised p-button-success mt-auto" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 lg:col-4">
+                    <div className="p-3 h-full">
+                        <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
+                            <div className="text-900 font-medium text-xl mb-2">ALIMENTOS</div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <div className="flex align-items-center">
+                                <span className="ml-2 font-medium text-600">Descripción</span>
+                            </div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Chart type="doughnut" data={chartData} options={lightOptions} />
+                            <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Button label="Gestionar Alimentos" className="p-button p-component p-button-raised p-button-success" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 lg:col-4">
+                    <div className="p-3 h-full">
+                        <div className="shadow-2 p-3 flex flex-column" style={{ borderRadius: '6px' }}>
+                            <div className="text-900 font-medium text-xl mb-2">GRAFICAS</div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <div className="flex align-items-center">
+
+                                <span className="ml-2 font-medium text-600">Descripción</span>
+                            </div>
+                            <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Chart type="doughnut" data={chartData} options={lightOptions} />
+                            <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
+                            <Button label="Gestionar Graficas" className="p-button p-component p-button-raised p-button-success" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div className="col-12 lg:col-4">
-            <div className="p-3 h-full">
-                <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
-                    <div className="text-900 font-medium text-xl mb-2">TERNEROS</div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <div className="flex align-items-center">
-                        <span className="ml-2 font-medium text-600">Descripción</span>
-                    </div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <Chart type="doughnut" data={chartData} options={lightOptions}  />
-                    <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <Button label="Gestionar Terneros" className="p-button p-component p-button-raised p-button-success" />
-                </div>
-            </div>
-        </div>
-
-        <div className="col-12 lg:col-4">
-            <div className="p-3 h-full">
-                <div className="shadow-2 p-3 flex flex-column" style={{ borderRadius: '6px' }}>
-                    <div className="text-900 font-medium text-xl mb-2">GUACHERAS</div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <div className="flex align-items-center">
-                        
-                        <span className="ml-2 font-medium text-600">Descripción</span>
-                    </div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-            <Chart type="doughnut" data={chartData} options={lightOptions} />
-                    <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <Button label="Gestionar Guacheras" className="p-button p-component p-button-raised p-button-success" />
-                </div>
-            </div>
-        </div>
-    </div>
-    
-    <div className="grid">
-        <div className="col-12 lg:col-4">
-            <div className="p-3 h-full">
-                <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
-                    <div className="text-900 font-medium text-xl mb-2">ENFERMEDADES</div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <div className="flex align-items-center">
-                        <span className="ml-2 font-medium text-600">Descripción</span>
-                    </div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <Chart type="doughnut" data={chartData} options={lightOptions} />
-                    <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300 mt-auto" />
-                    <Button label="Gestionar Enfermedades" className="p-button p-component p-button-raised p-button-success mt-auto" />
-                </div>
-            </div>
-        </div>
-
-        <div className="col-12 lg:col-4">
-            <div className="p-3 h-full">
-                <div className="shadow-2 p-3 h-full flex flex-column" style={{ borderRadius: '6px' }}>
-                    <div className="text-900 font-medium text-xl mb-2">ALIMENTOS</div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <div className="flex align-items-center">
-                        <span className="ml-2 font-medium text-600">Descripción</span>
-                    </div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <Chart type="doughnut" data={chartData} options={lightOptions} />
-                    <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <Button label="Gestionar Alimentos" className="p-button p-component p-button-raised p-button-success" />
-                </div>
-            </div>
-        </div>
-
-        <div className="col-12 lg:col-4">
-            <div className="p-3 h-full">
-                <div className="shadow-2 p-3 flex flex-column" style={{ borderRadius: '6px' }}>
-                    <div className="text-900 font-medium text-xl mb-2">GRAFICAS</div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <div className="flex align-items-center">
-                        
-                        <span className="ml-2 font-medium text-600">Descripción</span>
-                    </div>
-                    <hr className="my-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <Chart type="doughnut" data={chartData} options={lightOptions} />
-                    <hr className="mb-3 mx-0 border-top-1 border-bottom-none border-300" />
-                    <Button label="Gestionar Graficas" className="p-button p-component p-button-raised p-button-success" />
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-    
 
 
 
 
 
-  );
+    );
 
-  
+
 };
 
 export default Graficas;
